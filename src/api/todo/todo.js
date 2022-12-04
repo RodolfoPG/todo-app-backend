18b272e99fe82ee51852cf56d4612ca5ee05633d
Rodolfo P. Grião <103333634+RodolfoPG@users.noreply.github.com>
2022-12-04 20:01:48 -0300

    Corrigindo sintaxe que causou bugs

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -4,7 +4,7 @@ const mongoose = restful.mongoose
 const todoSchema = new mongoose.Schema({
   description: { type: String, require: true },
   done: { type: Boolean, require: true, default: false },
-  createdAt: { type: Date, defaul: Date.now }
+  createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = restful.model('Todo', todoSchema)
